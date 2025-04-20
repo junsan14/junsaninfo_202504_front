228635073ae539c9ca81bf69a66d4464a1f78c90
junsan14 <jusnanjunsan14@gmail.com>
2025-04-20 16:01:38 +0200

    register edit

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -25,7 +25,9 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         setErrors([])
 
         axios
-            .post('/register', props)
+            .post('/register', props,{
+                withCredentials: true,
+            })
             .then(() => mutate())
             .catch(error => {
                 if (error.response.status !== 422) throw error

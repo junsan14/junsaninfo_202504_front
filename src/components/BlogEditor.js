3cac417b72c7b29880f4fd0d18a27fd14f73f418
junsan14 <jusnanjunsan14@gmail.com>
2025-06-04 06:48:35 +0200

    0604

--- src/components/BlogEditor.js
+++ src/components/BlogEditor.js
@@ -64,7 +64,7 @@ export default function BlogEditor({postData}){
         is_top:isNew?0:post.is_top,
         is_featured:isNew?0:post.is_featured,
         published_at:isNew?formatinputDate(new Date()):post.published_at,
-        is_update:0,
+        is_update:1,
         is_continue:isNew?1:post.is_continue,
         is_restore:"false"
         })
